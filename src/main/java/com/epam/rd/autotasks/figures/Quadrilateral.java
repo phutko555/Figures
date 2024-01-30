c480a2e720bef57ed2d139e7fca01015ea07f054
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-01-30 07:44:53 +0000

    Update Quadrilateral.java

--- src/main/java/com/epam/rd/autotasks/figures/Quadrilateral.java
+++ src/main/java/com/epam/rd/autotasks/figures/Quadrilateral.java
@@ -1,31 +1,33 @@
 package com.epam.rd.autotasks.figures;
 
-class Triangle extends Figure {
+class Quadrilateral extends Figure {
     private Point a;
     private Point b;
     private Point c;
+    private Point d;
 
-    public Triangle(Point a, Point b, Point c) {
+    public Quadrilateral(Point a, Point b, Point c, Point d) {
         this.a = a;
         this.b = b;
         this.c = c;
+        this.d = d;
     }
 
     @Override
     public double area() {
-       
-       
-        return Math.abs((a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY()) + c.getX() * (a.getY() - b.getY())) / 2.0);
+        // Using the formula for the area of a convex quadrilateral divided into two triangles by a diagonal
+        return new Triangle(a, b, c).area() + new Triangle(a, c, d).area();
     }
 
     @Override
     public String pointsToString() {
-        return a.toString() + b.toString() + c.toString();
+        return a.toString() + b.toString() + c.toString() + d.toString();
     }
 
     @Override
     public Point leftmostPoint() {
-        Point[] points = {a, b, c};
+        // Return the leftmost point based on X coordinate
+        Point[] points = {a, b, c, d};
         Arrays.sort(points, (p1, p2) -> Double.compare(p1.getX(), p2.getX()));
         return points[0];
     }
