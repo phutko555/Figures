ded2fd0cc47705da3d0c407c53a6dae58cb9c2cc
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-01-31 00:35:53 +0000

    Update Quadrilateral.java

--- src/main/java/com/epam/rd/autotasks/figures/Quadrilateral.java
+++ src/main/java/com/epam/rd/autotasks/figures/Quadrilateral.java
@@ -1,6 +1,48 @@
-ackage com.epam.rd.autotasks.figures;
+package com.epam.rd.autotasks.figures;
 
-class Quadrilateral {
+class Quadrilateral extends Figure {
+    private Point[] vertices;
 
+    public Quadrilateral(Point a, Point b, Point c, Point d) {
+        this.vertices = new Point[]{a, b, c, d};
+    }
+
+    @Override
+    public double area() {
+        // Split the quadrilateral into two triangles using diagonal AC (vertices[0] and vertices[2])
+        Triangle triangle1 = new Triangle(vertices[0], vertices[1], vertices[2]);
+        Triangle triangle2 = new Triangle(vertices[0], vertices[2], vertices[3]);
+
+        return triangle1.area() + triangle2.area();
+    }
+
+    @Override
+    public String pointsToString() {
+        StringBuilder sb = new StringBuilder();
+        for (Point vertex : vertices) {
+            sb.append(String.format("(%s,%s)", vertex.getX(), vertex.getY()));
+        }
+        return sb.toString();
+    }
+
+    @Override
+    protected Point[] getVertices() {
+        return new Point[0];
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Quadrilateral[%s]", pointsToString());
+    }
+
+    @Override
+    public Point leftmostPoint() {
+        Point leftmost = vertices[0];
+        for (Point vertex : vertices) {
+            if (vertex.getX() < leftmost.getX()) {
+                leftmost = vertex;
+            }
+        }
+        return leftmost;
+    }
 }
-
