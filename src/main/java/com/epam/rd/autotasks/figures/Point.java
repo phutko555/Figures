dc3326ce17ff563727fede302f20d68403237d16
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-01-31 00:35:30 +0000

    Update Point.java

--- src/main/java/com/epam/rd/autotasks/figures/Point.java
+++ src/main/java/com/epam/rd/autotasks/figures/Point.java
@@ -17,4 +17,27 @@ class Point {
         return y;
     }
 
+    @Override
+    public String toString() {
+        return String.format("(%s,%s)", x, y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Point other = (Point) obj;
+        return Double.compare(other.x, x) == 0 && Double.compare(other.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        long xBits = Double.doubleToLongBits(x);
+        long yBits = Double.doubleToLongBits(y);
+        result = 31 * result + (int) (xBits ^ (xBits >>> 32));
+        result = 31 * result + (int) (yBits ^ (yBits >>> 32));
+        return result;
+    }
 }
