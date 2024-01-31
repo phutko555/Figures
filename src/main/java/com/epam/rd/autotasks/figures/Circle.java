59be4a95af05e642a7ea96bc87d3bac807b8d335
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-01-31 00:24:33 +0000

    Update Circle.java

--- src/main/java/com/epam/rd/autotasks/figures/Circle.java
+++ src/main/java/com/epam/rd/autotasks/figures/Circle.java
@@ -1,28 +1,6 @@
 package com.epam.rd.autotasks.figures;
 
-class Circle extends Figure {
-    private Point center;
-    private double radius;
+class Circle {
 
-    public Circle(Point center, double radius) {
-        this.center = center;
-        this.radius = radius;
-    }
-
-    @Override
-    public double area() {
-        // Using the formula for the area of a circle
-        return Math.PI * Math.pow(radius, 2);
-    }
-
-    @Override
-    public String pointsToString() {
-        return center.toString();
-    }
-
-    @Override
-    public Point leftmostPoint() {
-        // For a circle, any point on the circumference can be considered
-        return new Point(center.getX() - radius, center.getY());
-    }
 }
+
