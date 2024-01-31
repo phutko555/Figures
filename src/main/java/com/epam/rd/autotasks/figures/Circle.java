c9e9792100c4598736c68b8979674e93314ab012
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-01-31 00:34:45 +0000

    Update Circle.java

--- src/main/java/com/epam/rd/autotasks/figures/Circle.java
+++ src/main/java/com/epam/rd/autotasks/figures/Circle.java
@@ -1,6 +1,36 @@
 package com.epam.rd.autotasks.figures;
 
-class Circle {
+class Circle extends Figure {
+    private Point center;
+    private double radius;
 
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String pointsToString() {
+        return String.format("(%s,%s)", center.getX(), center.getY());
+    }
+
+    @Override
+    protected Point[] getVertices() {
+        return new Point[0];
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Circle[%s%s]", pointsToString(), radius);
+    }
+
+    @Override
+    public Point leftmostPoint() {
+        return center;
+    }
 }
-
