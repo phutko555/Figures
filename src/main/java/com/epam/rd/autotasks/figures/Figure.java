500c7c5d8bb4b2edccf783e6debabb3b4aadc8ce
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-02-05 13:00:19 +0000

    Update Figure.java

--- src/main/java/com/epam/rd/autotasks/figures/Figure.java
+++ src/main/java/com/epam/rd/autotasks/figures/Figure.java
@@ -1,26 +1,13 @@
 package com.epam.rd.autotasks.figures;
 
-import java.util.Arrays;
-
 abstract class Figure {
-
     public abstract double area();
 
-    public abstract Point leftmostPoint();
+    public abstract String pointsToString();
 
-    public String pointsToString() {
-        Point[] points = getVertices();
-        StringBuilder sb = new StringBuilder();
-        for (Point point : points) {
-            sb.append(point).append(",");
-        }
-        return sb.substring(0, sb.length() - 1);
-    }
-
-    protected abstract Point[] getVertices();
-
-    @Override
     public String toString() {
-        return String.format("%s[%s]", getClass().getSimpleName(), pointsToString());
+        return this.getClass().getSimpleName() + "[" + pointsToString() + "]";
     }
+
+    public abstract Point leftmostPoint();
 }
