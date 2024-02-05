ddf128b96699ba6d35e38c501b6e14357463ee6c
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-02-05 12:59:42 +0000

    Update Point.java

--- src/main/java/com/epam/rd/autotasks/figures/Point.java
+++ src/main/java/com/epam/rd/autotasks/figures/Point.java
@@ -1,43 +1,13 @@
 package com.epam.rd.autotasks.figures;
 
-class Point {
-    private double x;
-    private double y;
+abstract class Figure {
+    public abstract double area();
 
-    public Point(final double x, final double y) {
-        this.x = x;
-        this.y = y;
-    }
+    public abstract String pointsToString();
 
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    @Override
     public String toString() {
-        return String.format("(%s,%s)", x, y);
+        return this.getClass().getSimpleName() + "[" + pointsToString() + "]";
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-
-        Point other = (Point) obj;
-        return Double.compare(other.x, x) == 0 && Double.compare(other.y, y) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = 17;
-        long xBits = Double.doubleToLongBits(x);
-        long yBits = Double.doubleToLongBits(y);
-        result = 31 * result + (int) (xBits ^ (xBits >>> 32));
-        result = 31 * result + (int) (yBits ^ (yBits >>> 32));
-        return result;
-    }
+    public abstract Point leftmostPoint();
 }
