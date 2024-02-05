98f2f40aa8e0ada4bb2ca86778f3f7f73528de66
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-02-05 12:57:21 +0000

    Update Quadrilateral.java

--- src/main/java/com/epam/rd/autotasks/figures/Quadrilateral.java
+++ src/main/java/com/epam/rd/autotasks/figures/Quadrilateral.java
@@ -1,5 +1,3 @@
-package com.epam.rd.autotasks.figures;
-
 class Quadrilateral extends Figure {
     private Point[] vertices;
 
@@ -10,14 +8,14 @@ class Quadrilateral extends Figure {
     @Override
     public double area() {
         
-        Triangle triangle1 = new Triangle(vertices[0], vertices[1], vertices[2]);
-        Triangle triangle2 = new Triangle(vertices[0], vertices[2], vertices[3]);
-
+        Triangle triangle1 = new Triangle(vertices[0], vertices[1], vertices[3]);
+        Triangle triangle2 = new Triangle(vertices[1], vertices[2], vertices[3]);
         return triangle1.area() + triangle2.area();
     }
 
     @Override
     public String pointsToString() {
+      
         StringBuilder sb = new StringBuilder();
         for (Point vertex : vertices) {
             sb.append(String.format("(%s,%s)", vertex.getX(), vertex.getY()));
@@ -26,19 +24,11 @@ class Quadrilateral extends Figure {
     }
 
     @Override
-    public String toString() {
-        return String.format("Quadrilateral[%s]", pointsToString());
-    }
-
-    @Override
     public Point leftmostPoint() {
-        Point leftmost = vertices[0];
-        for (Point vertex : vertices) {
-            if (vertex.getX() < leftmost.getX()) {
-                leftmost = vertex;
-            }
-        }
-        return leftmost;
+    
+        return Arrays.stream(vertices)
+                .min((p1, p2) -> Double.compare(p1.getX(), p2.getX()))
+                .orElse(null);
     }
 }
 
